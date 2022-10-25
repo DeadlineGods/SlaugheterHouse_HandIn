b31019f51d530f48004b54eccfeb90f665921b84
koziara <fhdorpol.pl@gmail.com>
2022-10-25 13:28:10 +0200

    Commiting

---
+++ DataPersistance/src/main/java/com/example/datapersistance/DataPersistanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.datapersistance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataPersistanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataPersistanceApplication.class, args);
+	}
+
+}
---
+++ DataPersistance/src/test/java/com/example/datapersistance/DataPersistanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datapersistance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataPersistanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
